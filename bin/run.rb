4ab72be12d10e0fb19a145c9d6fea247dbcac21a
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-11 22:44:42 -0500

    adds mapquest api request for getting long and lat of cities and states

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,31 @@
 require_relative '../config/environment'
+require_relative '../lib/intro'
 require "pry"
 require "colorize"
-require_relative '../lib/intro'
+require "tty-prompt"
 
-response_string = RestClient.get("https://api.darksky.net/forecast/#{ENV['API_KEY']}/42.3601,-71.0589")
+
+puts "Please input your location in the following format (Town or City, State): "
+location = gets.chomp
+
+################# Using the MapQuest API gets the longitude and latitude of any town/state given ##################################
+map_string = RestClient.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAP_QUEST_API_KEY']}&location=#{location}")
+map_hash = JSON.parse(test_string)
+
+####################### Using Dark Star API we need to take the long, and lat from the above api and use it #####################
+response_string = RestClient.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/42.3601,-71.0589")
 response_hash = JSON.parse(response_string)
+
+
+################Example of using tty-prompt########################
+# prompt = TTY::Prompt.new
+#
+# prompt.select("Welcome User, please select an option") do |menu|
+#   menu.choice 'New York'
+#   menu.choice 'Boston'
+#   menu.choice 'Long Beach'
+#   menu.choice 'Detroit'
+#   menu.choice 'San Fransico'
+#   menu.choice 'Check New Location'
+# end
+###################################################################
