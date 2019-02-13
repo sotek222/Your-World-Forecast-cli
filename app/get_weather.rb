e3667fff8f007c5e43eecf041f5e655e014021f8
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-13 10:22:29 -0500

    adds delete locations, and user

--- app/get_weather.rb
+++ app/get_weather.rb
@@ -1,8 +1,10 @@
 
 
-def get_weather(coordinates)
+def get_weather(location_name, coordinates)
   response_string = RestClient.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{coordinates["lat"]},#{coordinates["lng"]}")
   response_hash = JSON.parse(response_string)
+  # binding.pry
+  puts "#{location_name}"
   puts "#{response_hash["currently"]["temperature"]} degrees"
   puts "#{response_hash["currently"]["summary"]}"
 end
--- bin/test_run.rb
+++ bin/test_run.rb
@@ -9,6 +9,12 @@ require "pry"
 require "colorize"
 require "tty-prompt"
 
+@sun_marker = "☼"
+
+def spacer
+  puts "\n\n"
+end
+
 def converter(location)
   map_string = RestClient.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAP_QUEST_API_KEY']}&location=#{location}")
   map_hash = JSON.parse(map_string)
@@ -25,57 +31,79 @@ end
 
 
 def main_menu
-prompt = TTY::Prompt.new
+  prompt = TTY::Prompt.new
 
-choice = prompt.select("Welcome #{@user.username}, where would you like to check the weather? ") do |menu|
-  menu.choice 'Look Up New Location'
-  menu.choice 'View Saved Locations'
-  menu.choice 'Exit'
-end
+  choice = prompt.select("Hello #{@user.username}, would you like to check the weather?", marker: @sun_marker) do |menu|
+    menu.choice 'Look Up New Location'
+    menu.choice 'View Saved Locations'
+    menu.choice 'Delete Saved Locations'
+    menu.choice 'Delete Account'
+    menu.choice 'Exit'
+  end
 
-if choice == 'Look Up New Location'
-  puts "Please input your location in the following format (Town or City, State): "
-  location_response = gets.chomp
-  location_hash = converter(location_response)
+  if choice == 'Look Up New Location'
+    puts "Please input your location: "
+    location_response = gets.chomp
+    location_hash = converter(location_response)
 
+    spacer
+    get_weather(location_hash["adminArea5"], location_hash["latLng"])
 
-  get_weather(location_hash["latLng"])
+    save_prompt = TTY::Prompt.new
 
-  save_prompt = TTY::Prompt.new
+    confirmation = save_prompt.yes?('Would you like to save this location?')
+    if confirmation
+      location = Location.find_or_create_by(city: location_hash["adminArea5"], country: location_hash["adminArea1"], latitude: location_hash["latLng"]["lat"], longitude: location_hash["latLng"]["lng"])
+      UserLocation.find_or_create_by(user_id: @user.id, location_id: location.id)
+      scroll_text('Saved Successfully')
+      puts "\n"
+      main_menu
+    else
+      main_menu
+    end
+  elsif choice == 'View Saved Locations'
+    select_prompt = TTY::Prompt.new
 
-  confirmation = save_prompt.yes?('Would you like to save this location?')
-  if confirmation
-    location = Location.find_or_create_by(city: location_hash["adminArea5"], country: location_hash["adminArea1"], latitude: location_hash["latLng"]["lat"], longitude: location_hash["latLng"]["lng"])
-    UserLocation.create(user_id: @user.id, location_id: location.id)
-    scroll_text('Saved Successfully')
-    puts "\n"
-    main_menu
+    select_prompt.select("Here are your saved locations:", marker: @sun_marker) do |menu|
+      menu.choice "Exit"
+      @user.locations.each do |location|
+        menu.choice "#{location.city}"
+      end
+    end
+  elsif choice == 'Delete Saved Locations'
+    delete_prompt = TTY::Prompt.new
+    delete_response = delete_prompt.select("Here are your saved locations please select a location to delete:", marker: @sun_marker) do |menu|
+      menu.choice "Exit"
+      @user.locations.each do |location|
+        menu.choice "#{location.city}"
+      end
+    end
+    if delete_response == "Exit"
+      main_menu
+    else
+      location = Location.find_by(city: delete_response)
+      UserLocation.where(location_id: location.id, user_id: @user.id).destroy_all
+      @user.locations.reload
+      main_menu
+    end
+  elsif choice == 'Delete Account'
+    delete_prompt = TTY::Prompt.new
+    response = delete_prompt.yes?('Are you sure you want to delete your account?')
+    if response == true
+      UserLocation.where(user_id: @user.id).destroy_all
+      User.where(id: @user.id).destroy_all
+      user_login
+    else
+      main_menu
+    end
   else
-    main_menu
+    puts "Have a Good Day!"
+    sleep 1.0
+    fork {exec "clear"}
+    exit
   end
-elsif choice == 'View Saved Locations'
-  @user.locations.map do |location|
-    puts location.city
-  end
-else
-  puts "Have a Good Day!"
-  exit
 end
-end
+
 
 user_login
 main_menu
-
-
-################Example of using tty-prompt########################
-# prompt = TTY::Prompt.new
-#
-# prompt.select("Welcome User, please select an option") do |menu|
-#   menu.choice 'New York'
-#   menu.choice 'Boston'
-#   menu.choice 'Long Beach'
-#   menu.choice 'Detroit'
-#   menu.choice 'San Fransico'
-#   menu.choice 'Check New Location'
-# end
-###################################################################
